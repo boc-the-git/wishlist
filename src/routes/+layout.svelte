b1ecce130b8d09b83cc378ebe42cbc31d4d7130e
Carter Mintey <cmintey8@gmail.com>
2024-08-05 22:09:11 -0500

    remove deprecated appshell from skeleton

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
     import "../app.postcss";
 
-    import type { ComponentEvents } from "svelte";
     import { afterNavigate, beforeNavigate } from "$app/navigation";
     import { page } from "$app/stores";
-    import { AppShell, Modal, Toast, storePopup, type ModalComponent, initializeStores } from "@skeletonlabs/skeleton";
+    import { Modal, Toast, storePopup, type ModalComponent, initializeStores } from "@skeletonlabs/skeleton";
     import { computePosition, autoUpdate, flip, shift, offset, arrow } from "@floating-ui/dom";
 
     import NavBar from "$lib/components/navigation/NavBar.svelte";
@@ -13,10 +12,8 @@
     import GroupSelectModal from "$lib/components/modals/GroupSelectModal.svelte";
     import InviteUserModal from "$lib/components/modals/InviteUserModal.svelte";
     import type { LayoutData } from "./$types";
-    import { onMount } from "svelte";
     import BottomTabs from "$lib/components/navigation/BottomTabs.svelte";
     import { isInstalled } from "$lib/stores/is-installed";
-    import PullToRefresh from "pulltorefreshjs";
     import { navItems } from "$lib/components/navigation/navigation";
     import Drawer from "$lib/components/Drawer.svelte";
     import CreateSystemUser from "$lib/components/modals/CreateSystemUser.svelte";
@@ -37,47 +34,18 @@
     let showNavigationLoadingBar = false;
     let documentTitle: string | undefined;
     let disabled = false;
-    let scrollPosition = 0;
 
     beforeNavigate(() => {
         showNavigationLoadingBar = true;
     });
 
-    afterNavigate((params) => {
+    afterNavigate(() => {
         showNavigationLoadingBar = false;
         documentTitle = document?.title;
         disabled = titleDisabledUrls.find((url) => $page.url.pathname.match(url)) !== undefined;
-        // scroll to top
-        const isNewPage = params.from?.url.pathname !== params.to?.url.pathname;
-        const elemPage = document.querySelector("#page");
-        if (isNewPage && elemPage !== null) {
-            elemPage.scrollTop = 0;
-        }
     });
 
     initializeStores();
-
-    const scrollHandler = (event: ComponentEvents<AppShell>["scroll"]) => {
-        //@ts-expect-error scrollTop doesn't exist on currentTarget
-        scrollPosition = event.currentTarget?.scrollTop;
-    };
-
-    onMount(() => {
-        if (window.matchMedia("(display-mode: standalone)").matches) {
-            $isInstalled = true;
-
-            const ptr = PullToRefresh.init({
-                mainElement: document.getElementById("main") as unknown as string,
-                distThreshold: 70,
-                resistanceFunction: (t) => Math.min(1, t / 4.5),
-                shouldPullToRefresh: () => scrollPosition === 0,
-                onRefresh() {
-                    window.location.reload();
-                }
-            });
-            return () => ptr.destroy();
-        }
-    });
 
     storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 
@@ -99,25 +67,23 @@
 
 <Drawer />
 
-<AppShell on:scroll={scrollHandler}>
-    <svelte:fragment slot="header">
+<div class="flex h-full w-full flex-col">
+    <header class="sticky top-0 z-10">
         {#if showNavigationLoadingBar}
             <NavigationLoadingBar />
         {/if}
         <NavBar {navItems} user={data.user} />
-    </svelte:fragment>
-    <!-- Router Slot -->
-    <div id="main" class="px-4 py-4 md:px-12 lg:px-32 xl:px-56">
+    </header>
+    <main class="px-4 py-4 md:px-12 lg:px-32 xl:px-56">
         {#if !$isInstalled && !disabled && documentTitle}
             <h1 class="h1 pb-2 md:pb-4">{documentTitle}</h1>
         {/if}
         <slot />
-    </div>
-
-    <svelte:fragment slot="footer">
+    </main>
+    <footer>
         <BottomTabs {navItems} user={data.user} />
-    </svelte:fragment>
-</AppShell>
+    </footer>
+</div>
 
 <Toast />
 <Modal components={modalComponentRegistry} />
